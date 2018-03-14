01f5003fbbc470a30c9e083ba07f5f92ebeaebc9
Senthil Kumar <sen.kumar2007@gmail.com>
2018-03-14 05:41:56 -0400

    realex payment driver changes to complete transaction

--- app/Ninja/PaymentDrivers/RealexHostedPaymentDriver.php
+++ app/Ninja/PaymentDrivers/RealexHostedPaymentDriver.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Ninja\PaymentDrivers;
 
 use Exception;
@@ -8,6 +7,7 @@ use App\Models\GatewayType;
 
 class RealexHostedPaymentDriver extends BasePaymentDriver
 {
+
     protected $customerReferenceParam = 'customerReference';
     protected $sourceReferenceParam = 'paymentMethodReference';
     public $canRefundPayments = true;
@@ -21,120 +21,38 @@ class RealexHostedPaymentDriver extends BasePaymentDriver
         return $types;
     }
 
-    public function tokenize()
+    protected function creatingPayment($payment, $paymentMethod)
     {
-        return true;
+        $payment->payer_id = $this->input['guid'];
+
+        return $payment;
+    }
+
+    public function completeOffsitePurchase($input)
+    {
+        if (!empty($input['guid'])) {
+            return $this->createPayment($input['guid']);
+        } else {
+            throw new Exception('Invalid Realex transaction');
+        }
     }
 
     protected function paymentDetails($paymentMethod = false)
     {
         $data = parent::paymentDetails($paymentMethod);
 
-        if (! $paymentMethod && ! empty($this->input['sourceToken'])) {
-            $data['token'] = $this->input['sourceToken'];
-        }
+        echo '<pre> in realex';
+
+        $data['hppCustomerCountry'] = 'US';
+        $data['hppCustomerFirstName'] = 'James';
+        $data['hppCustomerLastName'] = 'Mason';
+        $data['merchantResponseUrl'] = $data['returnUrl'];
+        $data['hppTxstatusUrl'] = $data['cancelUrl'];
+        $data['hppVersion'] = 2;
+        $data['comment1'] = 2;
+        $data['comment2'] = 2;
+        $data['testMode'] = true;
 
         return $data;
-    }
-
-    protected function checkCustomerExists($customer)
-    {
-        $response = $this->gateway()
-            ->fetchCustomer(['customerReference' => $customer->token])
-            ->send();
-
-        return $response->isSuccessful();
-    }
-
-    public function createToken()
-    {
-        $data = $this->paymentDetails();
-
-        if (($customer = $this->customer()) && $this->customer()->token) {
-            $customerReference = $customer->token;
-        } else {
-            $tokenResponse = $this->gateway()->createCustomer([
-                'firstName' => array_get($this->input, 'first_name') ?: $this->contact()->first_name,
-                'lastName' => array_get($this->input, 'last_name') ?: $this->contact()->last_name,
-                'company' => $this->client()->name,
-                'country' => $this->client()->country->iso_3166_3,
-                'primaryEmail' => $this->contact()->email,
-                'phoneDay' => $this->contact()->phone,
-            ])->send();
-            if ($tokenResponse->isSuccessful()) {
-                $customerReference = $tokenResponse->getCustomerReference();
-            } else {
-                return false;
-            }
-        }
-
-        if ($customerReference) {
-            $request = array(
-                'customerReference' => $customerReference,
-                'nameOnAccount' => $data['card']->getBillingFirstName() . ' ' . $data['card']->getBillingLastName(),
-                'addressLine1' => $data['card']->getBillingAddress1(),
-                'addressLine2' => $data['card']->getBillingAddress2(),
-                'city' => $data['card']->getBillingCity(),
-                'stateProvince' => $data['card']->getBillingState(),
-                'zipPostalCode' => $data['card']->getBillingPostcode(),
-                'country' => \App\Models\Country::where('iso_3166_2', '=', $data['card']->getBillingCountry())->firstOrFail()->iso_3166_3,
-            );
-
-            if ($this->isGatewayType(GATEWAY_TYPE_CREDIT_CARD)) {
-                $request['paymentToken'] = $this->input['sourceToken'];
-            }
-
-            $tokenResponse = $this->gateway->createPaymentMethod($request)->send();
-            if ($tokenResponse->isSuccessful()) {
-                $this->tokenResponse = $tokenResponse;
-            } else {
-                return false;
-            }
-        }
-
-        return parent::createToken();
-    }
-
-    public function creatingCustomer($customer)
-    {
-        $customer->token = $this->tokenResponse->getCustomerReference();
-
-        return $customer;
-    }
-
-    protected function creatingPaymentMethod($paymentMethod)
-    {
-        $response = $this->tokenResponse;
-
-        $paymentMethod->source_reference = $response->getPaymentMethodReference();
-
-        if ($this->isGatewayType(GATEWAY_TYPE_CREDIT_CARD)) {
-            $paymentMethod->payment_type_id = $this->parseCardType($response->getData()['cardBrand']);
-            $paymentMethod->last4 = $response->getData()['accountNumberLast4'];
-            $paymentMethod->expiration = sprintf(
-                '20%d-%d-01',
-                substr($response->getData()['expirationDate'], 2, 2),
-                substr($response->getData()['expirationDate'], 0, 2)
-            );
-        } else {
-            return null;
-        }
-
-        return $paymentMethod;
-    }
-
-    public function removePaymentMethod($paymentMethod)
-    {
-        parent::removePaymentMethod($paymentMethod);
-
-        $response = $this->gateway()->deletePaymentMethod([
-            'paymentMethodReference' => $paymentMethod->source_reference,
-        ])->send();
-
-        if ($response->isSuccessful()) {
-            return true;
-        } else {
-            throw new Exception($response->getMessage());
-        }
     }
 }
