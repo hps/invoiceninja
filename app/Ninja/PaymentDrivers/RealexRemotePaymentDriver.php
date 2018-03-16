ad003f4d54143b27ca1398f55ad31d9e5e6d847e
Senthil Kumar <sen.kumar2007@gmail.com>
2018-03-16 07:11:45 -0400

    hosted payment replaced by remote driver

--- app/Ninja/PaymentDrivers/RealexRemotePaymentDriver.php
+++ app/Ninja/PaymentDrivers/RealexRemotePaymentDriver.php
@@ -5,7 +5,7 @@ use Exception;
 use Session;
 use App\Models\GatewayType;
 
-class RealexHostedPaymentDriver extends BasePaymentDriver
+class RealexRemotePaymentDriver extends BasePaymentDriver
 {
 
     protected $customerReferenceParam = 'customerReference';
@@ -16,16 +16,17 @@ class RealexHostedPaymentDriver extends BasePaymentDriver
     {
         $types = [
             GATEWAY_TYPE_REALEX,
-            //GATEWAY_TYPE_CREDIT_CARD
+            GATEWAY_TYPE_CREDIT_CARD
         ];
-        //$this->accountGateway->gateway->provider = strtolower('Realex_Remote');
+              
         return $types;
     }
-    
-    
+
     protected function paymentDetails($paymentMethod = false)
-    {        
+    {
         $data = parent::paymentDetails($paymentMethod);
+
+        $data['transactionId'] = uniqid() . '-' . $data['transactionId'];
         
         if (!$this->isGatewayType(GATEWAY_TYPE_REALEX)) {
             return $data;
@@ -35,32 +36,44 @@ class RealexHostedPaymentDriver extends BasePaymentDriver
             $this->client()->country->iso_3166_2 :
             ($this->account()->country ? $this->account()->country->iso_3166_2 : false);
 
-        $data['transactionId'] = uniqid() . '-' . $data['transactionId'];
         $data['hppCustomerCountry'] = $country;
         $data['hppCustomerFirstName'] = $data['card']->getBillingFirstName();
         $data['hppCustomerLastName'] = $data['card']->getBillingLastName();
         $data['merchantResponseUrl'] = $data['returnUrl'];
         $data['hppTxstatusUrl'] = $data['returnUrl'];
         $data['comment1'] = 'Invoice Id: ' . $data['transactionId'];
-        
+
         //echo '<pre>';print_r($this->accountGateway->getConfig());die;
 
         return $data;
     }
+    
+    public function startPurchase($input = false, $sourceId = false){
+        if ($this->isGatewayType(GATEWAY_TYPE_REALEX)) {
+            $this->accountGateway->gateway->provider = 'Realex_Hosted';
+            $this->accountGateway->gateway->is_offsite = 1;
+        }
+        return parent::startPurchase($input, $sourceId);
+    }
 
     protected function creatingPayment($payment, $paymentMethod)
     {
-        $payment->payer_id = $this->input['guid'];
+        if ($this->isGatewayType(GATEWAY_TYPE_REALEX)) {
+            $payment->payer_id = $this->input['guid'];
+        }
 
         return $payment;
     }
 
     public function completeOffsitePurchase($input)
     {
-        if (!empty($input['guid'])) {
-            return $this->createPayment($input['guid']);
-        } else {
-            throw new Exception('Invalid Realex transaction');
+        if ($this->isGatewayType(GATEWAY_TYPE_REALEX)) {
+            if (!empty($input['guid'])) {
+                return $this->createPayment($input['guid']);
+            } else {
+                throw new Exception('Invalid Realex transaction');
+            }
         }
+        parent::completeOffsitePurchase($input);
     }
 }
